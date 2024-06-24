7cff7ff9c0d822d862a7a03c666b799b985b949c
Tonmay1555 <ahmmedtonmay1555@gmail.com>
2024-06-24 12:18:51 +0600

    clear awit

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    
     // await client.connect();
 
     const database = client.db("eduManage");
